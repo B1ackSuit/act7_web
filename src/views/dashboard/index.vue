e5144024768a4f6b8676c6a1c9b4540bc27b9fea
B1ackSuit <ean969648839@gmail.com>
2022-05-27 00:01:02 +0800

    演示版本，后续优化中

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">Activiti 流程管理系统</div>
+    <a href="http://localhost:8079/#/home"><el-button type="primary" style="width: 250px">返回OA系统</el-button> </a>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
